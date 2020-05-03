e48349ecb0896017a9f05ba000c8f057a4176627
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-05-03 13:27:56 -0700

    Added trivia question,answer and options

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,69 @@
 
 $(document).ready(function(){
 
+    var triviaGame = [
+        {
+            question: "What Christmas Ballet is the most famous of all?",
+            answer:    "The Nutcracker",
+            options:   ["Rudolph's Surprise","The Nutcracker", "Mr. and Mrs. Claus", "Frosty Goes to New York" ]
+
+        },
+
+        {
+            question: "Where was Mommy kissing Santa Claus?",
+            answer:    "Under the Mistletoe",
+            options:   ["On the Corner", "In the Bedroom", "Under the Mistletoe", "In a Dark Alley"]
+        },
+
+        {
+            question: "Who wrote the song \"Here Comes Santa Claus\"?",
+            answer:    "Gene Autry",
+            options:   ["Michael Jackson", "Gene Autry", "Persy Douglas", "Leroy Jones"]
+        },
+
+        {
+            question: "What does Alvin want for Christmas?",
+            answer:    "A Hula Hoop",
+            options:   ["An iPad", "A Bottle of Rum", "A Hula Hoop", "A New Car"]
+        },
+
+        {
+            question: "What should little children leave out for Santa on Christmas Eve?",
+            answer:    "Cookies and Milk",
+            options:   ["Cookies and Milk", "A Bottle of Wine", "Chewing Gum", "Chedder Cheese"]
+        },
+
+        {
+            question: "What is Frosty the Snowman's nose made of?",
+            answer:    "A Button",
+            options:   ["A Carrot", "A Potato", "A Button", "A Rock"]
+        },
+
+        {
+            question: "What is Ebenezer?",
+            answer:    "The Scrooge",
+            options:   ["The Milk Man", "The 23rd President", "The Scroogr", "Mrs. Claus's Secret Friend"]
+        },
+
+        {
+            question: "What color is the Grinch?",
+            answer:    "Green",
+            options:   ["Green", "Blue", "White", "Black"]
+        },
+
+        {
+            question: "Which reindeer's name starts with a \"B\" ?",
+            answer:   "Blitzen",
+            options:  ["Bart", "Burt", "Bodog", "Blitzen"]
+        },
+
+        {
+            question: "Which reindee does not belong below?",
+            answer:   "Roger",
+            options:  ["Dancer", "Comet", "Roger", "Dasher"]
+
+       }];
+
 
 
 
