164c4f13ca94456194c81d02fa4d6d8234ea2b41
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-05-05 12:58:47 -0700

    Delete an error syntax on click to check function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,77 +5,78 @@ $(document).ready(function(){
     var triviaGame = [
         {
             question: "What Christmas Ballet is the most famous of all?",
-            answer:    1,
+            answer:    "1",
             choice:   ["Rudolph's Surprise","The Nutcracker", "Mr. and Mrs. Claus", "Frosty Goes to New York" ]
 
         },
 
         {
             question: "Where was Mommy kissing Santa Claus?",
-            answer:    2,
+            answer:    "2",
             choice:   ["On the Corner", "In the Bedroom", "Under the Mistletoe", "In a Dark Alley"]
         },
 
         {
             question: "Who wrote the song \"Here Comes Santa Claus\"?",
-            answer:    1,
+            answer:    "1",
             choice:   ["Michael Jackson", "Gene Autry", "Persy Douglas", "Leroy Jones"]
         },
 
         {
             question: "What does Alvin want for Christmas?",
-            answer:    2,
+            answer:    "2",
             choice:   ["An iPad", "A Bottle of Rum", "A Hula Hoop", "A New Car"]
         },
 
         {
             question: "What should little children leave out for Santa on Christmas Eve?",
-            answer:    0,
+            answer:    "0",
             choice:   ["Cookies and Milk", "A Bottle of Wine", "Chewing Gum", "Chedder Cheese"]
         },
 
         {
             question: "What is Frosty the Snowman's nose made of?",
-            answer:    2,
+            answer:    "2",
             choice:   ["A Carrot", "A Potato", "A Button", "A Rock"]
         },
 
         {
             question: "What is Ebenezer?",
-            answer:    2,
+            answer:    "2",
             choice:   ["The Milk Man", "The 23rd President", "The Scroogr", "Mrs. Claus's Secret Friend"]
         },
 
         {
             question: "What color is the Grinch?",
-            answer:    0,
+            answer:    "0",
             choice:   ["Green", "Blue", "White", "Black"]
         },
 
         {
             question: "Which reindeer's name starts with a \"B\" ?",
-            answer:   3,
+            answer:   "3",
             choice:  ["Bart", "Burt", "Bodog", "Blitzen"]
         },
 
         {
             question: "Which reindee does not belong below?",
-            answer:   2,
+            answer:   "2",
             choice:  ["Dancer", "Comet", "Roger", "Dasher"]
 
        }];
-      var userAnswer = 0;
+    
       var correct = 0;
       var incorrect = 0;
       var unanswered = 0;
       var randomTrivia;
       var computerPick;
+      var userAnswer=0;
       var timer = 15;     //  Interval Demonstration ,  Set our time counter to 15.
       var intervalId;     //  Variable that will hold our interval ID when we execute ,   the "run" function
       var countQuestions = 0;
 
     //Hiding  Choice Div and Play Again Button
-    
+  
     $("#playagainBtn").hide();
      
    //Start Game
@@ -85,15 +86,19 @@ $(document).ready(function(){
     $("#startBtn").hide();
 
     //Display question and Multiple choice
+    //When user answer the question or time out then go to next question
     displayQuestion();
   
     //Start Time Remaining
     runTimer();
 
-  });
+
+
+
+
+  
     
-    //When user answer the question or time out then go to next question
-   
+    
     
 
     //If user pick the correct answer, show message ,audio sound and picture
@@ -130,12 +135,37 @@ $(document).ready(function(){
     var answerChoice = $("<div>");
     answerChoice.addClass("choiceDiv");
     answerChoice.html(computerPick.choice[i]);
+    answerChoice.attr("answerVal", i);
     $("#choicePage").append(answerChoice);
-
-    
    }
     
-   };
+  }
+  
+
+   /*$( "#choicePage" ).on( "click", function() {
+
+     userAnswer = ( $( this ).is( ".choiceDiv" ) );
+
+    if (userAnswer === computerPick.answer) {
+     alert("Correct");
+    } else if (userAnswer !== computerPick.answer){
+      alert("WTF");
+    } */
+
+
+  
+    
+      
+
+
+   
+
+    // TODO: If the count is the same as the length of the image array, reset the count to 0.
+    //if (countQuestions === triviaGame.length) {
+      // Show the result
+    //}
+
+
 
    
 
@@ -183,22 +213,7 @@ $(document).ready(function(){
 
   
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 
 
