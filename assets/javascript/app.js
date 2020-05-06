7be13bfc73b9d9ddb730a1fd3e15bbec69c4eb61
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-05-05 18:06:29 -0700

    Adding image into triviaGame array then add code to check when user answer and didn't answer the question the image will pop up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,61 +6,71 @@ $(document).ready(function(){
         {
             question: "What Christmas Ballet is the most famous of all?",
             choice:   ["Rudolph's Surprise","The Nutcracker", "Mr. and Mrs. Claus", "Frosty Goes to New York" ],
-            answer:    1
+            answer:    1,
+            image:    "assets/images/win.jpeg"
         },
 
         {
             question: "Where was Mommy kissing Santa Claus?",
             choice:   ["On the Corner", "In the Bedroom", "Under the Mistletoe", "In a Dark Alley"],
-            answer:    2
+            answer:    2,
+            image:    "assets/images/win.jpeg"
         },
 
         {
             question: "Who wrote the song \"Here Comes Santa Claus\"?",
             choice:   ["Michael Jackson", "Gene Autry", "Persy Douglas", "Leroy Jones"],
             answer:    1,
+            image:    "assets/images/win.jpeg"
         },
 
         {
             question: "What does Alvin want for Christmas?",
             choice:   ["An iPad", "A Bottle of Rum", "A Hula Hoop", "A New Car"],
             answer:    2,
+            image:    "assets/images/win.jpeg"
         },
 
         {
             question: "What should little children leave out for Santa on Christmas Eve?",
             choice:   ["Cookies and Milk", "A Bottle of Wine", "Chewing Gum", "Chedder Cheese"],
             answer:    0,
+            image:    "assets/images/win.jpeg"
         },
 
         {
             question: "What is Frosty the Snowman's nose made of?",
             choice:   ["A Carrot", "A Potato", "A Button", "A Rock"],
             answer:    2,
+            image:    "assets/images/win.jpeg"
         },
 
         {
             question: "What is Ebenezer?",
             choice:   ["The Milk Man", "The 23rd President", "The Scroogr", "Mrs. Claus's Secret Friend"],
             answer:    2,
+            image:    "assets/images/win.jpeg"
         },
 
         {
             question: "What color is the Grinch?",
             choice:   ["Green", "Blue", "White", "Black"],
             answer:    0,
+            image:    "assets/images/win.jpeg"
         },
 
         {
             question: "Which reindeer's name starts with a \"B\" ?",
             choice:  ["Bart", "Burt", "Bodog", "Blitzen"],
             answer:   3,
+            image:    "assets/images/win.jpeg"
         },
 
         {
             question: "Which reindee does not belong below?",
             choice:  ["Dancer", "Comet", "Roger", "Dasher"],
             answer:   2,
+            image:    "assets/images/win.jpeg"
 
        }];
     
@@ -70,7 +80,8 @@ $(document).ready(function(){
       var randomTrivia;
       var computerPick;
       var userAnswer="";
-      var timer = 15;     //  Interval Demonstration ,  Set our time counter to 15.
+      var timer = 15; //  Interval Demonstration ,  Set our time counter to 15.
+      var running = false;    
       var intervalId;     //  Variable that will hold our interval ID when we execute ,   the "run" function
       var countQuestions = 0;
 
@@ -84,36 +95,31 @@ $(document).ready(function(){
     //When click on start button , start button will disappear and turn to question page
     $("#startBtn").hide();
 
+    //Start Time Remaining
+    runTimer();
+    decrement();
+  
+   
     //Display question and Multiple choice
     //When user answer the question or time out then go to next question
     displayQuestion();
   
-    //Start Time Remaining
-    runTimer();
-
-
-
-
-
-  
-    
-    
     
 
-    //If user pick the correct answer, show message ,audio sound and picture
-    // to let user know that user pick the right answer , correct++
-
-    //If user pick incorrect answer, show message ,audio sound and picture 
-    //to let user know that user pick the wrong answer , incorrect++
-
-    //If time up user didn't pick the answer , show message ,audio sound and picture 
-    //to let user know that user pick didn't pick any answer , unanswered++
+    
    
     //Then go to the next question
 
-    //Until user answer all question then show the result + Play Again Button
+    //Check if user answer all question then show the result + Play Again Button
+
+   // Reset the Game
+
+
 
     // Create function for Correct Answer , Incorrect Answer and Unanswered the question
+
+    //If user pick the correct answer, show message ,audio sound and picture
+    // to let user know that user pick the right answer , correct++
     function correctAnswer(){
     $("#message").text("Correct!!");
     var correctSound = new Audio("assets/audio/Correct AnswerIdea Sound Effects.mp3");
@@ -125,7 +131,8 @@ $(document).ready(function(){
       
   };
   
-  // Create function a game loss,  player loss value losses + 1
+  //If user pick incorrect answer, show message ,audio sound and picture 
+  //to let user know that user pick the wrong answer , incorrect++
   function wrongAnswer(){
     $("#message").text("Your Answer is Incorrect!!");
     var incorrectSound = new Audio("assets/audio/Wrong Answer - Sound Effect [HD].mp3");
@@ -137,6 +144,8 @@ $(document).ready(function(){
     
   };
   
+  //If time up user didn't pick the answer , show message ,audio sound and picture 
+  //to let user know that user pick didn't pick any answer , unanswered++
   function unansweredQuestion() {
     unanswered++;
     $("#message").text("Time Out !! You failed to choose the answer");
@@ -145,13 +154,56 @@ $(document).ready(function(){
     //reset();
 }
 
+// Timer start count down, timer--
 
+function runTimer() {
+  if(!running){
+  intervalId = setInterval(decrement, 1000);
+  running = true;
+  }
+};
 
+//  The decrement function.
+function decrement() {
 
+  //When 5 second left alert with red color on text content
+  setTimeout(fiveSeconds, 1000 * 10);
+  fiveSeconds();
 
-   //Display question and Multiple choice
-   function displayQuestion(){
+  //  Show the time remaning
+  $("#time-remaining").html("<p><i class='fa fa-clock-o' style='padding: 20px;color:black;' aria-hidden='true'></i> Time Remaining :  " + timer + "</p>");
+
+  timer--;
+
+  
+   
+  //If time = 0 then stop the time
+
+  if (timer === 0) {
+    stop();
+  var timeUp = new Audio("assets/audio/gameover.mp3");
+      timeUp.play();
+      unansweredQuestion();
     
+  }
+};
+
+// 5 secound left alert with red color on text content
+
+function fiveSeconds() {
+  $("#time-remaining").html("<p style='color:red;'><i class='fa fa-clock-o' style='padding: 20px;color:red;' aria-hidden='true'></i> Time Remaining :  " + timer + "</p>");
+  console.log("5 seconds left");
+}
+
+//  The stop function
+function stop() {
+  running = false;
+  clearInterval(intervalId);
+};
+
+//Display question and Multiple choice
+function displayQuestion(){
+
      //Random Trivia Game
        randomTrivia = Math.floor(Math.random()*(triviaGame.length));
 
@@ -169,21 +221,23 @@ $(document).ready(function(){
    }
     
    $(".choiceDiv").on("click", function(){
-    userAnswer = parseInt( $( this ).attr( "answerVal" ) );
+    userAnswer = parseInt($( this ).attr( "answerVal" ));
 
     if (userAnswer === computerPick.answer) {
       correctAnswer();
+      $("#ansImage").append("<img src=" + computerPick.image + ">");
+     
      } else {
       wrongAnswer();
+      $("#ansImage").append("<img src=" + computerPick.image + ">");
      } 
 
 
      //Checking on console.log
      console.log(userAnswer);
-    
-
-
    });
+
+
 
   }
 
@@ -195,54 +249,8 @@ $(document).ready(function(){
     //}
 
 
-
-   
-
-   
-
-   
-
-  // Timer start count down, timer--
-
-   function runTimer() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-  };
-
-  //  The decrement function.
-  function decrement() {
-    timer--;
-     //
-     setTimeout(fiveSeconds, 1000 * 10);
-     fiveSeconds();
-
-    //  Show the time remaning
-    $("#time-remaining").html("<p><i class='fa fa-clock-o' style='padding: 20px;color:black;' aria-hidden='true'></i> Time Remaining :  " + timer + "</p>");
-
-    if (timer === 0) {
-      stop();
-    var timeUp = new Audio("assets/audio/gameover.mp3");
-        timeUp.play();
-        clearInterval(intervalId);
-        unansweredQuestion();
-      
-    }
-  };
-
-  // 5 secound left alert with red colro on text content
   
-  function fiveSeconds() {
-    $("#time-remaining").html("<p style='color:red;'><i class='fa fa-clock-o' style='padding: 20px;color:red;' aria-hidden='true'></i> Time Remaining :  " + timer + "</p>");
-    console.log("5 seconds left");
-  }
 
-  //  The stop function
-  function stop() {
-    clearInterval(intervalId);
-  };
-
-
-  
   
 });
 
