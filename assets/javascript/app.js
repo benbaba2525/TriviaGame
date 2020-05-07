e333038336e0714fb285aeaeffd345a27729abca
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-05-06 18:36:18 -0700

    Added function nextQuestion and check if play answer == index length then show the result

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -83,7 +83,10 @@ $(document).ready(function(){
     var timer = 15; //  Interval Demonstration ,  Set our time counter to 15.
     var running = false;    
     var intervalId;     //  Variable that will hold our interval ID when we execute ,   the "run" function
-    var countQuestions = 0;
+    var index = 0;
+    var newArray = [];
+    var holder = [];
+    var countQuestions = triviaGame.length;
 
   //Hiding  Choice Div and Play Again Button
 
@@ -95,28 +98,111 @@ $(document).ready(function(){
   //When click on start button , start button will disappear and turn to question page
   $("#startBtn").hide();
 
-  //Start Time Remaining
-  runTimer();
-  decrement();
-
- 
   //Display question and Multiple choice
   //When user answer the question or time out then go to next question
   displayQuestion();
 
-  
-
-  
- 
   //Then go to the next question
-
+for(var i=0; i < triviaGame.length; i++){
+ holder.push(triviaGame[i]);
+}
   //Check if user answer all question then show the result + Play Again Button
 
  // Reset the Game
 
+//Display question and Multiple choice
+function displayQuestion(){
+    //Start Time Remaining
+    runTimer();
+    decrement();
+
+     randomTrivia = Math.floor(Math.random()*(triviaGame.length));
+    
+      computerPick = triviaGame[randomTrivia];
+     //Show random question
+      $("#questionP").html("<p>" + computerPick.question + "</p>");
+      
+
+  //Show answer choices
+  for ( var i = 0; i < computerPick.choice.length; i++){
+   var answerChoice = $("<div>");
+   answerChoice.addClass("choiceDiv");
+   answerChoice.html(computerPick.choice[i]);
+   answerChoice.attr("answerVal", i);
+   $("#choicePage").append(answerChoice);
+  }
+   
+  $(".choiceDiv").on("click", function(){
+   userAnswer = parseInt($( this ).attr( "answerVal" ));
+ 
+   if (userAnswer === computerPick.answer) {
+     correctAnswer();
+    } else {
+     wrongAnswer();
+     
+    } 
+ 
+    //Checking on console.log
+    console.log(userAnswer);
+  });
+ 
+ 
+ 
+ }
 
 
-  // Create function for Correct Answer , Incorrect Answer and Unanswered the question
+ // Timer start count down, timer--
+
+function runTimer() {
+    if(!running){
+    intervalId = setInterval(decrement, 1000);
+    running = true;
+    }
+    };
+    
+//  The decrement function.
+function decrement() {
+    
+    //When 5 second left alert with red color on text content
+    setTimeout(fiveSeconds, 1000 * 10);
+    
+    
+    //  Show the time remaning
+    $("#time-remaining").html("<p><i class='fa fa-clock-o' style='padding: 20px;color:black;' aria-hidden='true'></i> Time Remaining :  " + timer + "</p>");
+    
+    timer--;
+     
+    //If time = 0 then stop the time
+    
+    if (timer === 0) {
+      stop();
+     
+    var timeUp = new Audio("assets/audio/gameover.mp3");
+        timeUp.play();
+        unansweredQuestion();
+       
+      
+    }
+    };
+    
+    // 5 secound left alert with red color on text content
+    
+    function fiveSeconds() {
+    $("#time-remaining").html("<p style='color:red;'><i class='fa fa-clock-o' style='padding: 20px;color:red;' aria-hidden='true'></i> Time Remaining :  " + timer + "</p>");
+    console.log("5 seconds left");
+   
+    }
+    
+    //  The stop function
+    function stop() {
+    running = false;
+    clearInterval(intervalId);
+    };
+    
+    
+    
+
+  // Create function to check Correct Answer , Incorrect Answer and Unanswered the question
 
   //If user pick the correct answer, show message ,audio sound and picture
   // to let user know that user pick the right answer , correct++
@@ -125,11 +211,15 @@ $(document).ready(function(){
   var correctSound = new Audio("assets/audio/Correct AnswerIdea Sound Effects.mp3");
       correctSound.play();
       correct++;
+      $("#ansImage").html("<img src=" + computerPick.image + ">");
       stop();
       $("#time-remaining").hide();
       $(".choiceDiv").hide();
-     
+      $("#message").show();
+      $("#ansImage").show();
+      nextQuestion();
       //reset();
+    
     
 };
 
@@ -140,11 +230,15 @@ function wrongAnswer(){
   var incorrectSound = new Audio("assets/audio/Wrong Answer - Sound Effect [HD].mp3");
       incorrectSound.play();
       incorrect++;
+      $("#ansImage").html("<img src=" + computerPick.image + ">");
       stop();
       $("#time-remaining").hide();
       $(".choiceDiv").hide();
-    
+      $("#message").show();
+      $("#ansImage").show();
+      nextQuestion();
       //reset ();
+     
   
 };
 
@@ -155,116 +249,49 @@ function unansweredQuestion() {
   $("#message").html("<span style='color:red';>Time Out !! You failed to choose the answer.</span> <br>  The Answer is  " + computerPick.choice[computerPick.answer]+"</p>");
   $("#time-remaining").hide();
   $(".choiceDiv").hide();
-  $("#ansImage").append("<img src=" + computerPick.image + ">");
-  
+  $("#ansImage").html("<img src=" + computerPick.image + ">");
+  $("#message").show();
+  $("#ansImage").show();
+  nextQuestion();
   //reset();
-}
-
-// Timer start count down, timer--
-
-function runTimer() {
-if(!running){
-intervalId = setInterval(decrement, 1000);
-running = true;
-}
-};
-
-//  The decrement function.
-function decrement() {
-
-//When 5 second left alert with red color on text content
-setTimeout(fiveSeconds, 1000 * 10);
-fiveSeconds();
-
-//  Show the time remaning
-$("#time-remaining").html("<p><i class='fa fa-clock-o' style='padding: 20px;color:black;' aria-hidden='true'></i> Time Remaining :  " + timer + "</p>");
-
-timer--;
-
-
- 
-//If time = 0 then stop the time
-
-if (timer === 0) {
-  stop();
-var timeUp = new Audio("assets/audio/gameover.mp3");
-    timeUp.play();
-    unansweredQuestion();
-   
   
 }
-};
-
-// 5 secound left alert with red color on text content
-
-function fiveSeconds() {
-$("#time-remaining").html("<p style='color:red;'><i class='fa fa-clock-o' style='padding: 20px;color:red;' aria-hidden='true'></i> Time Remaining :  " + timer + "</p>");
-console.log("5 seconds left");
-}
-
-//  The stop function
-function stop() {
-running = false;
-clearInterval(intervalId);
-};
-
-//Display question and Multiple choice
-function displayQuestion(){
-
-   //Random Trivia Game
-     randomTrivia = Math.floor(Math.random()*(triviaGame.length));
-
-     computerPick = triviaGame[randomTrivia];
-    //Show random question
-     $("#questionP").html("<p>" + computerPick.question + "</p>");
-
- //Show answer choices
- for ( var i = 0; i < computerPick.choice.length; i++){
-  var answerChoice = $("<div>");
-  answerChoice.addClass("choiceDiv");
-  answerChoice.html(computerPick.choice[i]);
-  answerChoice.attr("answerVal", i);
-  $("#choicePage").append(answerChoice);
- }
-  
- $(".choiceDiv").on("click", function(){
-  userAnswer = parseInt($( this ).attr( "answerVal" ));
-
-  if (userAnswer === computerPick.answer) {
-    correctAnswer();
-    $("#ansImage").append("<img src=" + computerPick.image + ">");
-   
-   } else {
-    wrongAnswer();
-    $("#ansImage").append("<img src=" + computerPick.image + ">");
-   } 
-
-
-   //Checking on console.log
-   console.log(userAnswer);
- });
-
-
-
-}
-
 
 
   // TODO: If the count is the same as the length of the image array, reset the count to 0.
   //if (countQuestions === triviaGame.length) {
     // Show the result
   //}
+function nextQuestion(){
+    newArray.push(computerPick);
+    triviaGame.splice(randomTrivia,1);
 
+    setTimeout(function(){
+        timer = 15;
+        $("#time-remaining").show();
+        $(".choiceDiv").empty();
+        $("#ansImage").empty();
+        $("#message").empty();
 
+// If the countQuestion is the same as index length, return the result if not continue displayQuestion.
+if((correct + incorrect + unanswered) === countQuestions){
+           $('#questionP').remove();
+           $('#time-remaining').remove();
+           $('#ansImage').remove();
+           $('#answerP').append('<h4 class= answersAll end>CORRECT ANSWERS: ' + correct + '</h4>');
+           $('#answerP').append('<h4 class= answersAll end>INCORRECT ANSWERS: ' + incorrect + '</h4>');
+           $('#answerP').append('<h4 class= answersAll end>UNANSWERED QUESTIONS: ' + unanswered + '</h4>');
 
+        }else{
+            displayQuestion();
+        }
 
+    },3000);
+};
+  
 
 });
 
 
 
-
-
-
-  
 });
